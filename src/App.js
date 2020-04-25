e393281d8eeeb3383d587a59ccbc958a34293796
David Lukumay <lukumaydavid@gmail.com>
2020-04-26 01:31:24 +0300

    reach router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { render } from "react-dom";
-import  SearchParams  from './SearchParams';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Link } from '@reach/router';
+import SearchParams from './SearchParams';
+import Details from './Details';
 
 const App = () => {
+	return (
+		<React.StrictMode>
+			<div>
+				<header>
+					<Link to="/">Adopt Me!</Link>
+				</header>
+				<Router>
+					<SearchParams path="/" />
+					<Details path="/details/:id" />
+				</Router>
+			</div>
+		</React.StrictMode>
+	);
+};
 
-
-  return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
-  )
-}
-
-render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById('root'));
