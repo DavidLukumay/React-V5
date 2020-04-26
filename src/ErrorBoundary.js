4fdf10958f67fd65f38b1b6e8b479cb0fb87275d
David Lukumay <lukumaydavid@gmail.com>
2020-04-26 19:17:53 +0300

    404 page redirect

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,11 +1,11 @@
 // mostly code from reactjs.org/docs/error-boundaries.html
 import React, { Component } from 'react';
-import { Link } from '@reach/router';
+import { Link, Redirect } from '@reach/router';
 
 class ErrorBoundary extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { hasError: false };
+		this.state = { hasError: false, redirect: false };
 	}
 	static getDerivedStateFromError() {
 		return { hasError: true };
@@ -13,7 +13,17 @@ class ErrorBoundary extends Component {
 	componentDidCatch(error, info) {
 		console.error('ErrorBoundary caught an error', error, info);
 	}
+
+	componentDidUpdate() {
+		if (this.state.hasError) {
+			setTimeout(() => this.setState({ redirect: true }), 5000);
+		}
+	}
+
 	render() {
+		if (this.state.redirect) {
+			return <Redirect to="/" noThrow />;
+		}
 		if (this.state.hasError) {
 			return (
 				<h1>
